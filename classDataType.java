205c6b502d9e55ddbf83abff4df8e8d13aee09b1
Abinaya <abimegala2005@gmail.com>
2025-06-14 09:31:03 +0530

    Data

---
+++ classDataType.java
@@ -0,0 +1,20 @@
+public class DataTypeDemo {
+    public static void main(string[]args)
+    {
+        int value1 = 9 / 2;
+        float value2 = 101 / 61 ;
+        double value3 = 10d/6d;
+        float value4 = 5/2.5f;
+        system.out.println("value 1="+ value1);
+        system.out.println("value 2="+ value2);
+        system.out.println("value 3="+ value3);
+        system.out.println("value 4="+ value4);
+
+        int marker = 512;
+
+        double percentage = marker*0.46f;
+
+        system.out.println("percentage :" + percentage);
+
+    }
+}
