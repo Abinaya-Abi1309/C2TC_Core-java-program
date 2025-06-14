d8527e37034468630891b0ec4870286380c5357c
Abinaya <abimegala2005@gmail.com>
2025-06-14 09:47:08 +0530

    Class

---
+++ ClassStatic.java
@@ -0,0 +1,19 @@
+public class CharDemo {
+    public static void main(string[] args)
+    {
+        char ch = 'a';
+        system.out.println(ch);
+
+        char ch1 = 65;
+        system.out.println(ch1);
+
+        char var1 = '\u00A7';
+        system.out.println(var1);
+
+        char var2 = '\u20AC';
+        system.out.println(var2);
+
+        int a = 'A';
+        system.out.println(a);
+    }
+}
