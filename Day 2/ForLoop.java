7bc80e55a8419b0104cbdbad0c7d337f5504d6a8
Abinaya <abimegala2005@gmail.com>
2025-06-15 13:35:39 +0530

    ForLoop

---
+++ Day 2/ForLoop.java
@@ -0,0 +1,11 @@
+public class ForLoopExample {
+public static void main(String[] args)
+{
+for(int i = 1; i<=1000; i++) 
+//(int i = 1000; i<=1;i--)
+{
+System.out.print("value of I:");
+}
+System.out.println(i) 
+}
+  }
