8f352ee7a75aabab20eb0baf2aa6441ca6acdad7
Abinaya <abimegala2005@gmail.com>
2025-06-15 12:53:54 +0530

    identifier

---
+++ Day 1/ClassIdentifierDemo.java
@@ -0,0 +1,9 @@
+public class IdentifierDemo {
+    public static void main(string[] args)
+    {
+        int number = 20;
+        systm.out.println("value of the number variable is : "+ number);
+        String studentName="Aniket";
+        systm.out.println("value is : "+ studentName);
+    }
+}
